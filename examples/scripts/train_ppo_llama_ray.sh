b7f77d990b8c8a85e37d95aae1ae1476ed590bff
IANNXANG <yx17180324@gmail.com>
2024-10-20 00:32:52 +0800

    /data

--- examples/scripts/train_ppo_llama_ray.sh
+++ examples/scripts/train_ppo_llama_ray.sh
@@ -31,7 +31,7 @@ ray job submit --address="http://127.0.0.1:8265" \
    --actor_learning_rate 5e-7 \
    --critic_learning_rate 9e-6 \
    --init_kl_coef 0.01 \
-   --prompt_data /pubshare/zy/cache/prompt-collection-v0.1/data \
+   --prompt_data OpenRLHF/prompt-collection-v0.1 \
    --input_key context_messages \
    --apply_chat_template \
    --normalize_reward \
