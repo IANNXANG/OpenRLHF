fc06aa5a963ceecc8226bea4f1c2dfb11192ca75
IANNXANG <yx17180324@gmail.com>
2024-10-20 17:16:14 +0800

    1212

--- examples/scripts/train_ppo_llama_ray.sh
+++ examples/scripts/train_ppo_llama_ray.sh
@@ -4,13 +4,13 @@ ray job submit --address="http://127.0.0.1:8265" \
    --runtime-env-json='{"working_dir": "/home/jovyan/notebook/zhouyang/OpenRLHF"}' \
    -- python3 -m openrlhf.cli.train_ppo_ray \
    --ref_num_nodes 1 \
-   --ref_num_gpus_per_node 1 \
+   --ref_num_gpus_per_node  \
    --reward_num_nodes 1 \
    --reward_num_gpus_per_node 1 \
    --critic_num_nodes 1 \
-   --critic_num_gpus_per_node 1 \
+   --critic_num_gpus_per_node 2 \
    --actor_num_nodes 1 \
-   --actor_num_gpus_per_node 1 \
+   --actor_num_gpus_per_node 2 \
    --vllm_num_engines 1 \
    --vllm_tensor_parallel_size 1 \
    --colocate_critic_reward \
